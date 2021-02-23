b9ee8e3631c7b0affd380f14a7f6f442d6640854
beries <benjamin.ries@capgemini.com>
2021-02-23 17:07:54 +0100

    mhhh

--- viking_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Vikings_tilemaps_026" tilewidth="32" tileheight="32" tilecount="60" columns="10">
- <image source="../../Downloads/Vikings_tilemaps_026.bmp" trans="ff00ff" width="320" height="200"/>
-</tileset>
